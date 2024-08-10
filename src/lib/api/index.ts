fb3a061aa325ab64348b390bdd42b1ff55d2f552
Mintae Kim <mintaekim@stamper.network>
2024-08-10 17:57:06 +0900

    [SOLSLAB-1] 기업 관련 API 정리

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,9 @@
 import { __Company } from './company';
 import { __Task } from './task';
+import { __Review } from './review';
 
 export namespace API {
 	export import Company = __Company;
 	export import Task = __Task;
+	export import Review = __Review;
 }
