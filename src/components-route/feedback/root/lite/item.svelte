93dad8cf0ea8ba330b6d666145ff8fc8f55e3b15
liupei8979 <mintaekim@stamper.network>
2024-09-10 19:30:11 +0900

    [SOLSLAB-6] 별 색상, 중앙정렬

--- src/components-route/feedback/root/lite/item.svelte
+++ src/components-route/feedback/root/lite/item.svelte
@@ -1,56 +1,83 @@
 <script lang="ts">
-    import { Modal } from '@src/components-global/modal';
-    interface Feedback {
-        rating: string;
-        feedback_content: string;
-    }
+	interface Feedback {
+		rating: string;
+		feedback_content: string;
+	}
 
-    export let feedbacks: Feedback[] = [];
+	export let feedbacks: Feedback[] = [];
 
-    function getStars(rating: string): string {
-        const num = parseInt(rating);
-        return '★'.repeat(num) + '☆'.repeat(5 - num);
-    }
+	function getStarsArray(rating: string) {
+		const num = parseInt(rating);
+		return {
+			filled: Array(num).fill('★'),
+			empty: Array(5 - num).fill('★')
+		};
+	}
 </script>
 
 {#if feedbacks.length > 0}
-    <table>
-        <thead>
-            <tr>
-                <th>별점</th>
-                <th>피드백</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each feedbacks as feedback}
-                <tr>
-                    <td>{getStars(feedback.rating)}</td>
-                    <td>{feedback.feedback_content}</td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
+	<table>
+		<thead>
+			<tr>
+				<th>별점</th>
+				<th>피드백</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each feedbacks as feedback}
+				<tr>
+					<td class="stars">
+						{#each getStarsArray(feedback.rating).filled as star}
+							<span class="filled">{star}</span>
+						{/each}
+						{#each getStarsArray(feedback.rating).empty as star}
+							<span class="empty">{star}</span>
+						{/each}
+					</td>
+					<td>{feedback.feedback_content ? feedback.feedback_content : '-'}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 {/if}
 
 <style lang="scss">
-    table {
-        width: 100%;
-        border-collapse: collapse;
-    }
+	table {
+		width: 100%;
+		border-collapse: collapse;
+	}
 
-    th,
-    td {
-        border: 1px solid var(--hq-base-0400);
-        color: var(--hq-base-2300);
-        padding: 8px;
-        text-align: left;
-    }
+	th,
+	td {
+		border: 1px solid var(--hq-base-0400);
+		color: var(--hq-base-2300);
+		padding: 8px;
+		text-align: left;
+	}
 
-    th {
-        background-color: var(--hq-base-1200);
-    }
+	th {
+		background-color: var(--hq-base-1200);
+	}
 
-    tr:hover {
-        background-color: var(--hq-base-0500);
-    }
+	tr:hover {
+		background-color: var(--hq-base-0500);
+	}
+
+	.stars {
+		text-align: center;
+		font-size: 0;
+
+		.filled,
+		.empty {
+			font-size: 1rem;
+		}
+
+		.filled {
+			color: yellow;
+		}
+
+		.empty {
+			color: gray;
+		}
+	}
 </style>
